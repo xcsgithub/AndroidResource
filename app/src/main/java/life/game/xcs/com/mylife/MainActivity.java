5f4910d93811f344ce50838956fa5d1cd4d962a9
xcs <18883259156@163.com>
2017-08-22 17:59:12 +0800

    添加toolBar

--- app/src/main/java/life/game/xcs/com/mylife/MainActivity.java
+++ app/src/main/java/life/game/xcs/com/mylife/MainActivity.java
@@ -1,13 +1,19 @@
 package life.game.xcs.com.mylife;
 
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
