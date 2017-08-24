c352fb3bec475aea939a8fb55c368a3cee155d30
xcs <18883259156@163.com>
2017-08-24 18:01:22 +0800

    搭建项目构建环境

--- app/src/main/java/life/game/xcs/com/mylife/MainActivity.java
+++ app/src/main/java/life/game/xcs/com/mylife/MainActivity.java
@@ -1,19 +1,54 @@
 package life.game.xcs.com.mylife;
 
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle("MyLife");
+       // toolbar.setSubtitle("financial");
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ab_android);
+        toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
+    }
+
+    private Toolbar.OnMenuItemClickListener onMenuItemClickListener = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            String msg = "";
+            switch (item.getItemId()){
+                case R.id.action_edit:
+                    msg += "Click edit";
+                    break;
+                case R.id.action_share:
+                    msg += "Click share";
+                    break;
+                case R.id.action_settings:
+                    msg += "Click settings";
+                    break;
+            }
+            if (!msg.equals("")){
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
