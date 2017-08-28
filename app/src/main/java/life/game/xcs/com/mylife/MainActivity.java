0222a89d78e479f8acd83db07afc00d3ce36835e
xcs <18883259156@163.com>
2017-08-28 18:05:09 +0800

    修改bottombar

--- app/src/main/java/life/game/xcs/com/mylife/MainActivity.java
+++ app/src/main/java/life/game/xcs/com/mylife/MainActivity.java
@@ -15,40 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setLogo(R.mipmap.ic_launcher);
-        toolbar.setTitle("MyLife");
-       // toolbar.setSubtitle("financial");
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ab_android);
-        toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
+
     }
 
-    private Toolbar.OnMenuItemClickListener onMenuItemClickListener = new Toolbar.OnMenuItemClickListener() {
-        @Override
-        public boolean onMenuItemClick(MenuItem item) {
-            String msg = "";
-            switch (item.getItemId()){
-                case R.id.action_edit:
-                    msg += "Click edit";
-                    break;
-                case R.id.action_share:
-                    msg += "Click share";
-                    break;
-                case R.id.action_settings:
-                    msg += "Click settings";
-                    break;
-            }
-            if (!msg.equals("")){
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-            }
-            return true;
-        }
-    };
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 }
